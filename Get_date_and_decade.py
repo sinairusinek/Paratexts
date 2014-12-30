b93d2a5093c4e8f02e121394b5e41b75cf0135e2
Sinai <sinai.rusinek@mail.huji.ac.il>
2014-12-30 17:49:55 +0200

    Date_and_decade_functions
    Helpful for processing messy date outputs

---
+++ Get_date_and_decade.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#can check if twice and a==b return
+#have to also exclude "after/not after x" and check in EEBO "later","earlier", "before"   
+#in ECCO there are two "--1798--"s which are misidentified as "range"
+
+import re, math
+
+YEAR_RE = re.compile(r'\d{4}')
+YEARquestion_RE = re.compile(r'\d{4}\?')  
+YEARange_RE = re.compile(r'\d{4}-|\/')
+YEARange2_RE = re.compile(r'(\d{4}).+(\d{4})')#???
+#YEARange_RE = re.compile(r'(\d{4}(-|\\)|(\d{4}){2})')
+
+def get_year(year_text):
+    match = YEAR_RE.search(year_text)
+    yearange = YEARange_RE.search(year_text) 
+    yearange2 = YEARange2_RE.search(year_text) 
+    yearquest = YEARquestion_RE.search(year_text)
+
+    
+    if yearange2:
+        return 2    #so 2 means that two dates were given as alternatives, or range. You might chose to examine those manually
+    elif yearange:
+        return 3    #3 means a range of years were given
+    elif yearquest:
+        return 0    #0 is when the date was marked with a question mark
+    elif match:
+        return int(match.group())
+    else: 	
+        return 4   #(which you should not get if all goes well)
+        
+    
+def todecades(intyear):
+    return (intyear//10) * 10
+
+#############  trial #############
+
+       
+def main ():
+    print(get_year("1942 to 1943"))
+    print(get_year("1942-43"))
+    print(get_year("1942?"))
+    print(get_year("1942"))
+    
+    print(todecades(1987))
+    print(todecades(3))
+
+if __name__=="__main__":
+    main()
+    
