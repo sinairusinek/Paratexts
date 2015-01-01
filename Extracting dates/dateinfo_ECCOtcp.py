cddb91a959b97fa900d8e535f1ee049c8d0bb1d2
Sinai <sinai.rusinek@mail.huji.ac.il>
2015-01-01 18:44:06 +0200

    for ECCO tcp files and headers

--- Extracting dates/dateinfo_ECCOtcp.py
+++ Extracting dates/dateinfo_ECCOtcp.py
@@ -7,12 +7,24 @@ import os, time
 import xml.etree.ElementTree as ET
 from GetDateDef import get_year, todecades
 
-text_fn ="EEBOdateinfo_%s" % time.strftime('%Y%m%d_%H%M%S')
+text_fn ="ECCOdateinfo_%s" % time.strftime('%Y%m%d_%H%M%S')
 text_file = open(text_fn + '.txt', 'w')
 
-def process_text(doc_id, input_el):
-    title = input_el.find('FILEDESC/TITLESTMT/TITLE')    
-    title_date = input_el.find('FILEDESC/SOURCEDESC/BIBLFULL/PUBLICATIONSTMT/DATE')    
+def get_header(doc_id):
+    path = os.path.join('headers', doc_id + '.hdr')
+    if not os.path.exists(path):
+        return None
+    tree = ET.parse(path)
+    return tree.getroot()
+
+def process_text(doc_id, input_el, text_el):
+    header_el = get_header(doc_id)
+    if header_el is None: 
+        print ('No header for %s' % doc_id)
+        return
+
+    title = header_el.find('FILEDESC/TITLESTMT/TITLE')    
+    title_date = header_el.find('FILEDESC/SOURCEDESC/BIBLFULL/PUBLICATIONSTMT/DATE')    
     if title_date is not None: 
         year = get_year(title_date.text)
         decade = todecades(year)
@@ -26,9 +38,10 @@ def process_xml(doc_id, fname):
         print ('Skipping '+ fname)
         return
     input_root = input_tree.getroot()
+    file_el = ET.SubElement(output_root, 'FILE', FILENAME=fname)
     
-    for input_el in input_root.findall('HEADER'):
-        process_text(doc_id, input_el)
+    for input_el in input_root.findall('EEBO/TEXT'):
+        process_text(doc_id, input_el, file_el)
 
 ##########
 
@@ -38,6 +51,11 @@ parser = argparse.ArgumentParser(description='Analyze the paratexts in historica
 parser.add_argument('--dir', default=ROOT_PATH, help='Directory of XMLs')
 
 args = parser.parse_args()
+
+output_root = ET.Element('PARATEXTS')
+output_tree = ET.ElementTree(output_root)
+
+paratext_type_dict = {}
 
 for dirpath, dirs, files in os.walk(args.dir):
     for fname in files:
